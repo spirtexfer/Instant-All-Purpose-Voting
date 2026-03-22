b0fd496e83747d50d1dfb8184b461390221508a2
spirtexfer <spirtexfer@gmail.com>
2026-03-22 20:59:08 +0800

    Fix TypeScript and vite config for GitHub Pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env['GITHUB_PAGES'] ? '/Instant-All-Purpose-Voting/' : '/',
+  base: '/Instant-All-Purpose-Voting/',
   server: {
     port: 5173,
     proxy: {
